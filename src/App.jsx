1518991183e532513320424e15b326af4b16538d
xcxdae <vadimdenega664@gmail.com>
2026-03-26 05:59:42 +0100

    Fix Netlify publish directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
-import Button from "./components/Button";
-import Input from "./components/Input";
-
 function App() {
-  const handleClick = () => {
-    alert("Кнопку натиснуто!");
-  };
-
-  const handleInputChange = (event) => {
-    console.log("Значення поля:", event.target.value);
-  };
-
-  return (
-    <div>
-      <h1>Тест компонентів</h1>
-
-      <Input
-        placeholder="Введіть текст"
-        type="text"
-        onChange={handleInputChange}
-      />
-
-      <Input
-        placeholder="Введіть пароль"
-        type="password"
-        onChange={handleInputChange}
-      />
-
-      <Button text="Натисни мене" type="button" onClick={handleClick} />
-    </div>
-  );
+  return <h1>ПРАЦЮЄ</h1>;
 }
 
 export default App;
